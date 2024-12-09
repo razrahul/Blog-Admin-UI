acc2bc776c7cdd8ae621915b81892415eb67800e
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-12-10 00:02:06 +0530

    resolve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-<<<<<<< HEAD
-import React, { lazy, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-const Navbar = lazy(() => import('./containers/Nav/Navbar'));
-// import Navbar from './containers/Nav/Navbar';
-const UsersPage = lazy(() => import('./pages/userPage/UsersPage'))
-const AddBlogPage = lazy(() => import('./pages/AddBlog/AddBlogPage'))
-const BlogListPage = lazy(() => import('./pages/BlogListPage/BlogListPage'))
-const ContactPage = lazy(() => import('./pages/Contact/ContactPage'))
-const LoginPage = lazy(() => import('./components/Login/Login'))
-import './App.scss';
-=======
-import React, { useState } from "react";
+import React, { lazy, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./containers/Nav/Navbar";
-import UsersPage from "./pages/userPage/UsersPage";
-import AddBlogPage from "./pages/AddBlog/AddBlogPage";
-import BlogListPage from "./pages/BlogListPage/BlogListPage";
-import ContactPage from "./pages/Contact/ContactPage";
-import LoginPage from "./components/Login/Login";
-import AddSubtitlePage from "./components/AddTitle/AddSubTitlePage";
+
+// Lazy load components
+const Navbar = lazy(() => import("./containers/Nav/Navbar"));
+const UsersPage = lazy(() => import("./pages/userPage/UsersPage"));
+const AddBlogPage = lazy(() => import("./pages/AddBlog/AddBlogPage"));
+const BlogListPage = lazy(() => import("./pages/BlogListPage/BlogListPage"));
+const ContactPage = lazy(() => import("./pages/Contact/ContactPage"));
+const LoginPage = lazy(() => import("./components/Login/Login"));
+const AddSubtitlePage = lazy(() =>
+  import("./components/AddTitle/AddSubTitlePage")
+);
+
 import "./App.scss";
->>>>>>> 620296637b0b68d6fc13eb512b48ff60de2d5e0c
 
 const App = () => {
-  // State to track if the user is logged in
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // Function to handle login
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
@@ -40,8 +30,7 @@ const App = () => {
         ) : (
           <>
             <div className="content-fix">
-              {/* Left Navbar */}
-              <Navbar />
+              <Navbar /> {/* Left Navbar */}
             </div>
 
             {/* Right-Side Dynamic Content */}
@@ -55,10 +44,6 @@ const App = () => {
                   element={<AddSubtitlePage />}
                 />
                 <Route path="/contact" element={<ContactPage />} />
-                <Route
-                  path="/add-subtitle/:blogId"
-                  element={<AddSubtitlePage />}
-                />
               </Routes>
             </div>
           </>
